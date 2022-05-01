38b57906498011f45b74dfed819d234891105b6f
b09505030 <73883522+b09505030@users.noreply.github.com>
2022-05-01 12:46:11 +0800

    Add files via upload

---
+++ final_project_game/3dgaming/src/com/mime/minefront/GameApplet.java
@@ -0,0 +1,29 @@
+//package com.mime.minefront;
+//
+//
+////import java.applet.Applet;
+//import java.applet.*;
+//import java.awt.BorderLayout;
+//
+//@SuppressWarnings("removal")
+//public class GameApplet extends Applet {
+//	private static final long serialVersionUID = 1L;
+//	
+//	private Display display = new Display();
+//	/**
+//	 * similar to main[String[] args]
+//	 */
+//	public void init() {
+//		setLayout(new BorderLayout());
+//		add(display);
+//	}
+//	
+//	public void start() {
+//		display.start();
+//	}
+//	
+//	public void stop() {
+//		display.stop();
+//	}
+//
+//}
